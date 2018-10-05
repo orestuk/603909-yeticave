ada7196ef6393e580b867f742ecb313eb1dba6cc
Keks <keks@htmlacademy.ru>
2018-10-05 16:49:38 +0300

    :cat:

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = 'Орест'; // укажите здесь ваше имя
+$user_name = ''; // укажите здесь ваше имя
 $user_avatar = 'img/user.jpg';
 ?>
 <!DOCTYPE html>
@@ -29,26 +29,23 @@ $user_avatar = 'img/user.jpg';
 
         <nav class="user-menu">
 
-            <?php
-            if ($is_auth == 1) {
-                ?>
-                <div class="user-menu__image">
-                    <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
-                </div>
-                <div class="user-menu__logged">
-                    <p><?=$user_name; ?></p>
-                </div>
-            <?php } else { ?>
-                <ul class="user-menu__list">
-                    <li class="user-menu__item">
-                        <a href="#">Регистрация</a>
-                    </li>
-                    <li class="user-menu__item">
-                        <a href="#">Вход</a>
-                    </li>
-                </ul>
-            <?php } ?>
-
+        <?php if ($is_auth): ?>
+          <div class="user-menu__image">
+            <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
+          </div>
+          <div class="user-menu__logged">
+            <p></p>
+          </div>
+        <?php else: ?>
+          <ul class="user-menu__list">
+            <li class="user-menu__item">
+              <a href="#">Регистрация</a>
+            </li>
+            <li class="user-menu__item">
+              <a href="#">Вход</a>
+            </li>
+          </ul>
+        <?php endif; ?>
 
         </nav>
     </div>
