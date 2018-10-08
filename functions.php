6b7d09422cff8c3a90ad55803d52c07eb2090ca1
Orest <orko.yarko@gmail.com>
2018-10-08 23:06:49 +0300

    Модуль 3 задание 1

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+
+
+function format_price($amount)
+{
+
+    $amount = ceil($amount);
+    $result = strval($amount);
+    $string_length = mb_strlen($result);
+    if ($amount > 1000) {
+        $result = mb_substr($result, 0, $string_length - 3 ).' '.mb_substr($result, $string_length - 4, 3);
+    }
+    return $result.' '.'₽';
+}
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require($name);
+
+    $result = ob_get_clean();
+
+    return $result;
+}
+
+function esc($str) {
+    $text = htmlspecialchars($str);
+    return $text;
+}
